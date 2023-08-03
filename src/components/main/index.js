0b64a455eb5ab477d0f08fd8f710823061aa268d
Hélio Schneider <helioschndr@gmail.com>
2023-08-03 20:29:53 -0300

    main content

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -3,6 +3,7 @@ import "./style.css"
 
 import Logo from "../.././img/logo-gojo.png"
 import Dice from "../.././img/dice.png"
+import Book from "../.././img/book.png"
 
 
 function index() {
@@ -17,6 +18,25 @@ function index() {
             <h1>Dos dados é feito o mundo</h1>
             <img src={Dice} alt="" />
             <button>Saiba Mais</button>
+        </div>
+        <div className="section-2">
+            <h1>Jujutsu Man</h1>
+            <p>Um emocionante RPG de mesa criado para proporcionar diversão entre amigos. Neste universo vibrante, a vida pulsa intensamente, repleto de risos compartilhados, mas também de lágrimas derramadas diante das perdas e derrotas enfrentadas juntos.</p>
+        </div>
+        <div className="section-3">
+          <h1>Jujutsu Man Lore</h1>
+          <div className='buttons-img'>
+            <div>
+              <button>Arcos</button>
+              <button>Lugares</button>
+              <button>Clãs</button>
+              <button>Personagens</button>
+            </div>
+            <div>
+              <img src={Book} alt="" />
+            </div>
+          </div>
+
         </div>
 
     </>
